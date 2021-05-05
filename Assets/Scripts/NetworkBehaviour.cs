9854032ba131373eb0f123f5b129396a2300adb5
Thomais Vasilopoulou <thomaisv97@gmail.com>
2021-05-05 12:13:41 +0300

    Posting random coordinates and prediction label

--- Assets/Scripts/NetworkBehaviour.cs
+++ Assets/Scripts/NetworkBehaviour.cs
@@ -3,14 +3,24 @@
 // https://github.com/reneschulte/WinMLExperiments/
 
 using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.Networking;
+using System.Net.Http;
+using System.Net;
+using System.Threading;
+// using Windows.Storage.Streams;
+// using Windows.Web.Http;
 
 public class NetworkBehaviour : MonoBehaviour
 {
     // Public fields
-    public float ProbabilityThreshold = 0.5f;
+    public float ProbabilityThreshold = 0.3f;
     public Vector2 InputFeatureSize = new Vector2(224, 224);
     public Text StatusBlock;
 
@@ -18,6 +28,8 @@ public class NetworkBehaviour : MonoBehaviour
     private NetworkModel _networkModel;
     private MediaCaptureUtility _mediaCaptureUtility;
     private bool _isRunning = false;
+
+    private static ManualResetEvent allDone = new ManualResetEvent(false);
 
     #region UnityMethods
     async void Start()
@@ -34,13 +46,13 @@ public class NetworkBehaviour : MonoBehaviour
             // Configure camera to return frames fitting the model input size
             try
             {
-                Debug.Log("Creating MediaCaptureUtility and initializing frame reader.");
+                UnityEngine.Debug.Log("Creating MediaCaptureUtility and initializing frame reader.");
                 _mediaCaptureUtility = new MediaCaptureUtility();
                 await _mediaCaptureUtility.InitializeMediaFrameReaderAsync(
                     (uint)InputFeatureSize.x, (uint)InputFeatureSize.y);
                 StatusBlock.text = $"Camera started. Running!";
 
-                Debug.Log("Successfully initialized frame reader.");
+                UnityEngine.Debug.Log("Successfully initialized frame reader.");
             }
             catch (Exception ex)
             {
@@ -50,24 +62,28 @@ public class NetworkBehaviour : MonoBehaviour
 
             // Run processing loop in separate parallel Task, get the latest frame
             // and asynchronously evaluate
-            Debug.Log("Begin performing inference in frame grab loop.");
-
+            UnityEngine.Debug.Log("Begin performing inference in frame grab loop.");
             _isRunning = true;
             await Task.Run(async () =>
             {
+                
                 while (_isRunning)
                 {
                     if (_mediaCaptureUtility.IsCapturing)
                     {
                         using (var videoFrame = _mediaCaptureUtility.GetLatestFrame())
                         {
+
                             await EvaluateFrame(videoFrame);
+ 
+
                         }
                     }
                     else
                     {
                         return;
                     }
+                    
                 }
             });
 #endif 
@@ -75,7 +91,7 @@ public class NetworkBehaviour : MonoBehaviour
         catch (Exception ex)
         {
             StatusBlock.text = $"Error init: {ex.Message}";
-            Debug.LogError($"Failed to start model inference: {ex}");
+            UnityEngine.Debug.LogError($"Failed to start model inference: {ex}");
         }
     }
 
@@ -96,20 +112,66 @@ public class NetworkBehaviour : MonoBehaviour
         {
             // Get the current network prediction from model and input frame
             var result = await _networkModel.EvaluateVideoFrameAsync(videoFrame);
-
             // Update the UI with prediction
-            UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+            UnityEngine.WSA.Application.InvokeOnAppThread(async () =>
             {
-                StatusBlock.text = $"Label: {result.PredictionLabel} " +
-                $"Probability: {Math.Round(result.PredictionProbability, 3) * 100}% " +
-                $"Inference time: {result.PredictionTime} ms";
-            }, false);
+                    StatusBlock.text = $"Prediction: {result.PredictionLabel}, " +
+                    $"Probability: {Math.Round(result.PredictionProbability, 3) * 100}% " +
+                    $"Inference time: {result.PredictionTime} ms";
+                    
+                    await TryPostJsonAsync(result.PredictionLabel);
+            }
+            , false);
+
         }
         catch (Exception ex)
         {
-            Debug.Log($"Exception {ex}");
+            UnityEngine.Debug.Log($"Exception {ex}");
+        }
+    }
+#endif
+
+    private async Task TryPostJsonAsync(string predLabel)
+    {
+        try
+        {
+            double [] coordinates = new double[2];
+            coordinates[0] = GetRandomNumber(0.0f, 50.0f);
+            coordinates[1] = GetRandomNumber(0.0f, 50.0f);
+
+            // Construct the HttpClient and Uri. This endpoint is for test purposes only.
+            HttpClient httpClient = new HttpClient();
+            Uri uri = new Uri("http://eagle5.di.uoa.gr:8082/topics/SCP476");
+
+            if(predLabel == "None" || predLabel == "No prediction exceeded probability threshold.") return;
+            else predLabel = predLabel.Substring(0, predLabel.Length - 1);
+
+            // Construct the JSON to post.
+            string json = "{\"records\":[{\"value\":{\"coordinates\":[" + string.Join(",", coordinates) + "], \"prediction\": " + predLabel + " }}]}";
+            var content = new System.Net.Http.StringContent(json, UnicodeEncoding.UTF8, "application/vnd.kafka.json.v2+json");
+
+            // Post the JSON and wait for a response.
+            HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(uri, content);
+
+            // Make sure the post succeeded, and write out the response.
+            httpResponseMessage.EnsureSuccessStatusCode();
+            var httpResponseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+        }
+        catch (Exception ex)
+        {
+            Write out any exceptions.
+            Debug.WriteLine(ex);
         }
     }
 
-#endif
+
+
+    public double GetRandomNumber(double minimum, double maximum)
+    { 
+        System.Random random = new System.Random();
+        return random.NextDouble() * (maximum - minimum) + minimum;
+    }
+    
+
+
 }
